9d57ce79a5a3a3ef0b54bef7ab134d0c9ecedc79
Satvik Sinha <satvik.sinha@cctech.co.in>
2025-10-15 19:19:24 +0530

    commit3

--- Arrays and Strings/Q3.cpp
+++ Arrays and Strings/Q3.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main(){
     int arr[5]={1,2,3,4,5};
-    
+    int n=5;
+     int start=0;
+     int end=n-1;
+     while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+
+     }
+     cout<<"Reversed Array is: "<<endl;
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "<<endl;
+     }
+     return 0;
+
 }
